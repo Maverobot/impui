d6b09f0e066254b7293fa42d369656c6019e6264
Maverobot <quzhengrobot@gmail.com>
2020-01-31 01:35:35 +0100

    ADD: TODOS and formatted

--- cmake/ClangTools.cmake
+++ cmake/ClangTools.cmake
@@ -9,7 +9,8 @@ endif()
 find_program(CLANG_TIDY_PROG clang-tidy DOC "'clang-tidy' executable")
 if(CLANG_TIDY_PROG AND NOT TARGET tidy)
   if(NOT CMAKE_EXPORT_COMPILE_COMMANDS)
-    message(WARNING "Invoke Catkin/CMake with '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
+    message(
+      WARNING "Invoke Catkin/CMake with '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
                      to generate compilation database for 'clang-tidy'.")
   endif()
   add_custom_target(tidy)
@@ -20,7 +21,10 @@ find_program(PARALLEL_PROG parallel DOC "'parallel' executable")
 if(PARALLEL_PROG)
   set(PARALLEL_DELIMITER ":::")
 else()
-  message(WARNING "parallel is not installeld. To enable parallelization of clang-tidy, try with 'sudo apt install parallel'.")
+  message(
+    WARNING
+      "parallel is not installeld. To enable parallelization of clang-tidy, try with 'sudo apt install parallel'."
+  )
   set(PARALLEL_PROG "")
   set(PARALLEL_DELIMITER "")
 endif()
@@ -31,20 +35,22 @@ function(add_format_target _target)
   endif()
   cmake_parse_arguments(ARG "" "" "FILES" ${ARGN})
 
-  add_custom_target(format-${_target}
+  add_custom_target(
+    format-${_target}
     COMMAND ${CLANG_FORMAT_PROG} -i ${ARG_FILES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
     COMMENT "Formatting ${_target} source code with clang-format"
-    VERBATIM
-  )
+    VERBATIM)
   add_dependencies(format format-${_target})
 
-  add_custom_target(check-format-${_target}
-    COMMAND ${CLANG_FORMAT_PROG} -output-replacements-xml ${ARG_FILES} | grep "<replacement " > /tmp/log && exit 1 || exit 0
+  add_custom_target(
+    check-format-${_target}
+    COMMAND
+      ${CLANG_FORMAT_PROG} -output-replacements-xml ${ARG_FILES} | grep
+      "<replacement " > /tmp/log && exit 1 || exit 0
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
     COMMENT "Checking ${_target} code formatting with clang-format"
-    VERBATIM
-  )
+    VERBATIM)
   add_dependencies(check-format check-format-${_target})
 endfunction()
 
@@ -54,21 +60,27 @@ function(add_tidy_target _target)
   endif()
   cmake_parse_arguments(ARG "" "" "FILES;DEPENDS" ${ARGN})
 
-  add_custom_target(tidy-${_target}
-    COMMAND ${PARALLEL_PROG} ${CLANG_TIDY_PROG} -fix -p=${CMAKE_BINARY_DIR} ${PARALLEL_DELIMITER} ${ARG_FILES}
+  add_custom_target(
+    tidy-${_target}
+    # TODO: "parallel" can have multiple fixes at the same position, which
+    # should be fixed.
+    # TODO: after tidy fix, a clang-format should be performed automatically.
+    COMMAND ${PARALLEL_PROG} ${CLANG_TIDY_PROG} -fix -p=${CMAKE_BINARY_DIR}
+            ${PARALLEL_DELIMITER} ${ARG_FILES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
     DEPENDS ${ARG_DEPENDS}
     COMMENT "Running clang-tidy for ${_target}"
-    VERBATIM
-  )
+    VERBATIM)
   add_dependencies(tidy tidy-${_target})
 
-  add_custom_target(check-tidy-${_target}
-  COMMAND ${PARALLEL_PROG} ${CLANG_TIDY_PROG} -p=${CMAKE_BINARY_DIR} ${PARALLEL_DELIMITER} ${ARG_FILES} | grep . && exit 1 || exit 0
+  add_custom_target(
+    check-tidy-${_target}
+    COMMAND
+      ${PARALLEL_PROG} ${CLANG_TIDY_PROG} -p=${CMAKE_BINARY_DIR}
+      ${PARALLEL_DELIMITER} ${ARG_FILES} | grep . && exit 1 || exit 0
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
     DEPENDS ${ARG_DEPENDS}
     COMMENT "Running clang-tidy for ${_target}"
-    VERBATIM
-  )
+    VERBATIM)
   add_dependencies(check-tidy check-tidy-${_target})
 endfunction()
