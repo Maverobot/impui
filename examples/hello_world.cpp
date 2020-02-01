afb33668fddc34c21539ee30659593d373a50b0e
Maverobot <quzhengrobot@gmail.com>
2020-02-02 00:58:13 +0100

    REMOVE: unused variable

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -6,7 +6,6 @@ int main(int /*unused*/, char** /*unused*/) {
   impui::Canvas canvas(1280, 720, "Dear ImGui GLFW+OpenGL2 example", nullptr, nullptr);
 
   // Our state
-  bool show_demo_window = true;
   bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
