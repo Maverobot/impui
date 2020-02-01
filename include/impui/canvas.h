9ac77e74d1f58bacca0e118ad2d951eb6a4f8a5c
Maverobot <quzhengrobot@gmail.com>
2020-02-01 21:45:43 +0100

    CHANGE: include "" -> include<>

--- include/impui/canvas.h
+++ include/impui/canvas.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl2.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl2.h>
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
 #endif
