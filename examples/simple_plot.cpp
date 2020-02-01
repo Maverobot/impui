bbef2d68a72c81e5b4c2813dbb986b7447b72a9f
Maverobot <quzhengrobot@gmail.com>
2020-02-01 20:36:34 +0100

    CHANGE: simplify example

--- examples/simple_plot.cpp
+++ examples/simple_plot.cpp
@@ -1,6 +1,6 @@
-
 #include <imgui_plot.h>
 #include <impui/canvas.h>
+#include <impui/plot.hpp>
 #include <impui/plot_data.hpp>
 
 #include <cmath>
@@ -14,54 +14,39 @@ int main(int /*unused*/, char** /*unused*/) {
     return 1;
   }
 
-  impui::PlotData data_map(100);
+  impui::PlotData plot_data(100);
 
   bool show_plot = false;
   float value = 0.001;
   // Main loop
   while (!canvas.shouldClose()) {
     // update data
-    data_map.append("position", std::sin(value));
-    data_map.append("velocity", std::cos(value));
-    data_map.append("acceleration", -std::sin(value));
+    plot_data.append("position", std::sin(value));
+    plot_data.append("velocity", std::cos(value));
+    plot_data.append("acceleration", -std::sin(value));
     value += 0.1f;
 
     canvas.poll();
     canvas.frameStart();
+
     // 1. Show a simple window
-    canvas.showWindow("ImViz", [&data_map, &show_plot] {
-      static float f = 0.0f;
-
-      ImGui::Text("This is a simple visualization demo.");
-
-      ImGui::SliderFloat("float", &f, 0.0f,
-                         1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
-
+    canvas.showWindow("ImViz", [&plot_data, &show_plot] {
       if (ImGui::Button("Reset")) {
-        data_map.clearAll();
+        plot_data.clearAll();
       }
-
+      ImGui::SameLine();
       ImGui::Checkbox("show plot", &show_plot);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     });
 
     // 2. Show another simple window.
     if (show_plot) {
-      canvas.showWindow(
-          "Plot",
-          [&data_map] {
-            ImGui::PlotLines("position", data_map.data("position"), data_map.len("position"), 0,
-                             nullptr, -1, 1, ImVec2(0, 80));
-
-            ImGui::PlotLines("velocity", data_map.data("velocity"), data_map.len("velocity"), 0,
-                             nullptr, -1, 1, ImVec2(0, 80));
-
-            ImGui::PlotLines("acceleration", data_map.data("acceleration"),
-                             data_map.len("acceleration"), 0, nullptr, -1, 1, ImVec2(0, 80));
-          },
-          &show_plot, ImGuiWindowFlags_AlwaysAutoResize);
+      canvas.showWindow("Plot",
+                        [plot_data = std::as_const(plot_data)] {
+                          impui::plot(plot_data, "position");
+                          impui::plot(plot_data, "velocity");
+                          impui::plot(plot_data, "acceleration");
+                        },
+                        &show_plot, ImGuiWindowFlags_AlwaysAutoResize);
     }
 
     canvas.render();
