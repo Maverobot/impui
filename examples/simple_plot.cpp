1401cc110435d8523f1258970332de20b8bb967b
Zheng Qu <quzhengrobot@gmail.com>
2020-02-15 20:52:26 +0100

    ADD: printing loop frequency (fps)

--- examples/simple_plot.cpp
+++ examples/simple_plot.cpp
@@ -2,10 +2,13 @@
 #include <impui/canvas.h>
 #include <impui/simple_plotter.h>
 
+#include <chrono>
 #include <cmath>
 #include <iostream>
 
 #include <cstdio>
+
+using namespace std::chrono_literals;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -18,6 +21,7 @@ int main(int argc, char* argv[]) {
   float value = 0.001;
 
   // Main loop
+  auto last = std::chrono::steady_clock::now();
   for (;;) {
     // update data
     plotter.add({{"position", std::sin(value)},
@@ -27,6 +31,12 @@ int main(int argc, char* argv[]) {
     plotter.add("eigen_vec", mat);
     plotter.plot();
     value += 0.1f;
+    auto now = std::chrono::steady_clock::now();
+    auto interval =
+        std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>(now - last)
+            .count();
+    last = now;
+    std::cout << "loop frequency: " << 1 / interval << std::endl;
   }
 
   return 0;
