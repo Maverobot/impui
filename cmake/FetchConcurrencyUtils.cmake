f2611767f66d0303bc12899a7f20e522f4cce8e5
Zheng Qu <quzhengrobot@gmail.com>
2020-04-10 14:44:12 +0200

    fix: concurrency_utils fetch

--- cmake/FetchConcurrencyUtils.cmake
+++ cmake/FetchConcurrencyUtils.cmake
@@ -2,10 +2,16 @@ include(FetchContent)
 FetchContent_Declare(
   concurrency_utils
   GIT_REPOSITORY https://github.com/Maverobot/concurrency_utils.git
-  GIT_TAG v0.0.1)
+  GIT_TAG v0.1)
 FetchContent_GetProperties(concurrency_utils)
 if(NOT concurrency_utils_POPULATED)
   FetchContent_Populate(concurrency_utils)
-  message(STATUS ${concurrency_utils_SOURCE_DIR})
-  add_subdirectory(${concurrency_utils_SOURCE_DIR})
 endif()
+
+# Create concurrency_utils library
+find_package(Threads REQUIRED)
+add_library(concurrency_utils INTERFACE)
+target_include_directories(
+  concurrency_utils INTERFACE
+  $<BUILD_INTERFACE:${concurrency_utils_SOURCE_DIR}/include>)
+target_link_libraries(concurrency_utils INTERFACE Threads::Threads)
